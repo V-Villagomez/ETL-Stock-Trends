aa81974a8b2a81b282281170fd8bb1cb69a53348
Vanessa Villagomez <villagomez.v@outlook.com>
2021-09-23 08:52:51 -0700

    modify finance_trends sql file

--- Database_Tables_SQL/finance_trends.sql
+++ Database_Tables_SQL/finance_trends.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS finance_trends;
 CREATE TABLE finance_trends (
 	id_number INT,
 	symbol VARCHAR(20),
-	region VARCHAR (20)
+	region VARCHAR (20),
 	startInterval DATE, 
 	jobTimestamp TIMESTAMP
 );
